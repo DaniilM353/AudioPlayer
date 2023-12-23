94623e8c31158b183bd4f73b06cbd3c0fbcddfce
DaniilM353 <d.maksimov@xlab-game.com>
2023-12-23 10:19:30 +0300

    Hotfix

--- lib/widgets/audio_info.dart
+++ lib/widgets/audio_info.dart
@@ -13,12 +13,12 @@ class AudioInfo extends StatelessWidget {
         ),
         const SizedBox(height: 30),
         const Text(
-          'Ukulele',
+          'Акулель',
           style: TextStyle(fontSize: 30),
         ),
         const SizedBox(height: 20),
         const Text(
-          'Benjamin Tissot',
+          'Исполнитель: Максимов',
           style: TextStyle(
             fontSize: 16,
             color: Colors.red,
